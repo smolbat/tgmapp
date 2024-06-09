e62a1a5ce1a47da136376a3cc351faf60eed8fd5
Андрей Битюцкий <smolbat@yandex.ru>
2024-06-09 17:38:26 +0300

    Add expand to all screen

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ let selectedRow = -1;
 
 // Инициализация Телеграм
 Telegram.WebApp.ready();
+// Расширяем МиниАпп на весь экран
+Telegram.WebApp.expand()
 
 // Show main button
 Telegram.WebApp.MainButton.setParams({
