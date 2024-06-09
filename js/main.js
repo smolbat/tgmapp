6cf82b3c28bee753692b4cc613069d31b68d9f9f
Андрей Битюцкий <smolbat@yandex.ru>
2024-06-09 16:55:21 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+var noSleep = new NoSleep(); // необходим, чтобы телефон не "засыпал"
+
+const PHONETICS = 1;
+const TRANSCRIPTION = 2;
+let phoneticType = PHONETICS;
+let songName = 'ENG000001';
+let jsonSongDataPath = 'songs/titles/' + songName + '.json';
+let songAudioPath = 'songs/' + songName + '.mp3';
+let timeShiftArray = [];
+let parsedSongData = [];
+let selectedRow = -1;
+
+// Инициализация Телеграм
+Telegram.WebApp.ready();
+
+// Show main button
+Telegram.WebApp.MainButton.setParams({
+    text: 'Play'
+});
+Telegram.WebApp.MainButton.show();
+
+
+let timeWhenAnimationStops = performance.now();
+
+let audioPlayer = new Audio();
+audioPlayer.src = songAudioPath;
+
+(async () => { // обертка, так как внутри есть await функции
+    // запрашиваем данные из внешнего файла
+    let jsonResponse = await fetch(jsonSongDataPath);
+    let songData = await jsonResponse.json();
+    let result = parseSongData(songData);
+    parsedSongData = result.words;
+    timeShiftArray = result.timeshift;
+    // рисуем субтитры
+    drawSubtitles(parsedSongData, phoneticType);
+    // и скролим их максимально вверх
+    $('.songPage1').animate({ scrollTop: 0 }, 1000);
+})();
+
+
+//  Обработка нажатия кнопки Фонетика
+$('#btnPhonetics').click(function() {
+    if (phoneticType == PHONETICS) {
+        phoneticType = TRANSCRIPTION;
+        $('#btnPhonetics').text('Транскрипция');
+    } else {
+        phoneticType = PHONETICS;
+        $('#btnPhonetics').text('Фонетика');
+    }
+    reDrawSubtitles(parsedSongData, phoneticType);
+});
+
+
+// Функция навигации по песне путем выбора строки песни
+$('#mySongList').click(function(event) {
+    let targetElement = event.target.closest('li');
+    selectedRow = $(targetElement).index();
+    audioPlayer.currentTime = timeShiftArray[selectedRow][0] / 1000;
+    selectRow(selectedRow);
+});
+
+
+////////////////////////////
+//      Аудиомодуль
+///////////////////////////
+// $('#btnPlay').click(function() {
+//     audioPlayer.paused ? audioPlayer.play() : audioPlayer.pause();
+// });
+
+Telegram.WebApp.MainButton.onClick(function() {
+    if (audioPlayer.paused) {
+        Telegram.WebApp.MainButton.setParams({
+            text: 'Play'
+        });
+    } else {
+        Telegram.WebApp.MainButton.setParams({
+            text: 'Pause'
+        });        
+    }
+    audioPlayer.paused ? audioPlayer.play() : audioPlayer.pause();
+});
+
+
+// функция, выделяющая строки во время проигрывания песни
+audioPlayer.ontimeupdate = function() {
+        let curTime = audioPlayer.currentTime;
+        // $('#btnPlay').text(curTime + '   TIME = ' + timeShiftArray[selectedRow + 1][0] / 1000);
+        selected = getSelectedRowAndWord(curTime);
+        selectedRow = selected.currentRow;
+        selectRow(selectedRow);
+        selectWord(selected);
+}
+
+// при выходе со страницы или перезагрузке удаляем аудио объкт
+// чтобы он не кешировался в браузере
+//
+// возможно функцию надо переработать!!!!
+window.onbeforeunload = function() {
+    noSleep.disable();
+    audioPlayer = null;
+    delete audioPlayer;
+    return
+}
---
+++ js/myFunctions.js
@@ -0,0 +1,117 @@
+// Фукция распарсивания json файла
+function parseSongData(songData) {
+    let songArray = songData.words.map(function(elem) {
+        return ({
+            text: elem.text,
+            start: elem.start,
+        })
+    });
+    let text = songData.text.split(' ');
+    let transcript = songData.transcript.split(' ');
+    let ru_transcript = songData.ru_transcript.split(' ');
+    let ru_translate = songData.ru_translate.split(' ');
+    // ru_translate.map(function(str) { return str.replace(/_/g, ' ') });
+    let resultArr = [];
+    let timeShiftArr = [];
+    let tsArr = [];
+    let arr = [];
+    for (i in songArray) {
+        if (songArray[i].text.slice(-1) != '_') {
+            arr.push([text[i], transcript[i], ru_transcript[i], ru_translate[i]]);
+            tsArr.push(songArray[i].start);
+        } else {
+            arr.push([text[i], transcript[i], ru_transcript[i], ru_translate[i]]);
+            resultArr.push(arr);
+            arr = [];
+            tsArr.push(songArray[i].start);
+            timeShiftArr.push(tsArr);
+            tsArr = [];
+        }
+    }
+    let result = { words: resultArr, timeshift: timeShiftArr };
+    return result;
+}
+
+// Функция рисования субтитров
+function drawSubtitles(songData, phonetic) {
+    let myHtmlElement = '';
+    songData.forEach(function(rowSong) {
+        myHtmlElement += drawOneRowSubtitles(rowSong, phonetic);
+    });
+    // Рисуем то, что создали в HTML
+    $('#mySongList').empty();
+    $(myHtmlElement).appendTo('#mySongList');
+}
+
+// Функция получения HTML текста одной строки субтитров
+function drawOneRowSubtitles(rowSong, phonetic) {
+    let myHtmlElement = '<li class = "item-content"><div class = "item-inner"><table>';
+    let text = '</tr>';
+    let transcript = '</tr>';
+    let ru_transcript = '</tr>';
+    let ru_translate = '</tr>';
+    for (element of rowSong) {
+        text += '<td>' + element[0] + '</td>';
+        transcript += '<td>' + element[1] + '</td>';
+        ru_transcript += '<td>' + element[2] + '</td>';
+        ru_translate += '<td>' + element[3] + '</td>';
+    }
+    text += '</tr>';
+    transcript += '</tr>';
+    ru_transcript += '</tr>';
+    ru_translate += '</tr>';
+    let middleElement = "";
+    if (phonetic != 1) { middleElement = transcript; } else { middleElement = ru_transcript; }
+
+    myHtmlElement += text + middleElement + ru_translate;
+    myHtmlElement += '</table></div></li>';
+    return myHtmlElement;
+}
+
+// Функция обновления субтитров для фонетики
+function reDrawSubtitles(songData, phonetic) {
+    for (i = 0; i < $('#mySongList li').length; ++i) {
+        let innerHTML = '';
+        let index = 1;
+        if (phonetic != 1) { index = 1 } else { index = 2 };
+        for (k in songData[i]) {
+            innerHTML += '<td>' + songData[i][k][index] + '</td>';
+        }
+        $('#mySongList tbody:eq(' + i + ') > tr:eq(1)')[0].innerHTML = innerHTML;
+    }
+}
+
+function getSelectedRowAndWord(currentTime) {
+    let curRow;
+    let curWordS;
+    curRow = timeShiftArray.findLastIndex(tsArr => tsArr[0] <= currentTime * 1000);
+    curWord = timeShiftArray[curRow].findLastIndex(arr => arr <= currentTime * 1000);
+    return { currentRow: curRow, currentWord: curWord };
+}
+
+function selectRow(selectedRow) {
+    // сбрасываем цвет всех выделеных слов
+    // так как последнее слово в строке не сбрасывается после 
+    // выделения следующей строки
+    $('#mySongList td').css('color', '');
+    // выставляем цвет выделенной строки
+    $('#mySongList li').css('background', '');
+    $('#mySongList li:eq(' + selectedRow + ')').css('background', 'silver');
+    let now = performance.now();
+    if (now > timeWhenAnimationStops) {
+        scrollToCenter(selectedRow);
+        timeWhenAnimationStops = now + 700;
+    }
+}
+
+function selectWord(selected) {
+    $('#mySongList tbody:eq(' + selected.currentRow + ') > tr:eq(1) > td').css('color', '');
+    $('#mySongList tbody:eq(' + selected.currentRow + ') > tr:eq(1) > td:eq(' + selected.currentWord + ')').css('color', 'SteelBlue');
+}
+
+function scrollToCenter(selectedRow) {
+    topShift = $('#mySongList')[0].offsetTop +
+        selectedRow * $('#mySongList li')[0].offsetHeight -
+        ($('.songPage1')[0].offsetHeight - $('#mySongList li')[0].offsetHeight) / 2;
+    $('.songPage1').animate({ scrollTop: topShift }, 700);
+}
