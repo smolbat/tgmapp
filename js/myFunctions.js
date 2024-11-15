47e5f6c3ecedfc3d470dafac6389b8b7a80ff97a
smolbat <33392352+smolbat@users.noreply.github.com>
2024-11-15 20:45:08 +0300

    Update myFunctions.js

--- js/myFunctions.js
+++ js/myFunctions.js
@@ -38,7 +38,7 @@ function parseSongData(songData) {
 
 // Функция рисования субтитров
 function drawSubtitles(songData, phonetic) {
-    let myHtmlElement = '<div>'+window.location.hash.slice(1)+'</div>';
+    let myHtmlElement = '<div>'+window.location.hash+'</div>';
     songData.forEach(function(rowSong) {
         myHtmlElement += drawOneRowSubtitles(rowSong, phonetic);
     });
